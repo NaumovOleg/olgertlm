d89b7a2ec1e4c2eb27f1f87281b655d5bb842021
Oleg <keeperoleg26@gmail.com>
2025-05-17 22:36:06 +0300

    dddddwwww

--- train.py
+++ train.py
@@ -6,21 +6,21 @@ from config import Config
 
 X, y, tokenizer, vocab_size = load_data(Config.DATA_PATH, maxlen=Config.MAXLEN)
 
-orcestrate = ModelFactory(vocab_size)
-orcestrate.load()
-orcestrate.compile()
+model_factory = ModelFactory(vocab_size)
+model_factory.load()
+model_factory.compile()
 
 
 print("Start fitting------------------>...")
-orcestrate.model.fit(
+model_factory.model.fit(
     X,
     y,
     epochs=Config.EPOCHS,
     batch_size=Config.BATCH_SIZE,
     callbacks=[HFPushCallback()],
-    initial_epoch=orcestrate.last_epoch,
+    initial_epoch=model_factory.last_epoch,
 )
 
-orcestrate.model.save("./saved/gpt.weights.keras")
+model_factory.model.save("./saved/gpt.weights.keras")
 
 print("Модель обучена и сохранена. Готова к генерации текста.")
