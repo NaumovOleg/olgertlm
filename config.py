73a8ba40658376e47d0c07e47f3ab8d1489a9b4f
Oleg <keeperoleg26@gmail.com>
2025-05-20 20:27:09 +0300

    wip

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config:
-    DATA_PATH = os.getenv("DATA_PATH", "")
+    DATA_PATH = os.getenv("DATA_PATH", "datasets/big.txt")
     MAXLEN = int(os.getenv("MAXLEN", "20"))
     NUM_LAYERS = int(os.getenv("NUM_LAYERS", "1"))
     EMBED_DIM = int(os.getenv("EMBED_DIM", "4"))
@@ -13,6 +13,10 @@ class Config:
     FF_DIM = int(os.getenv("FF_DIM", "128"))
     EPOCHS = int(os.getenv("EPOCHS", "10"))
     BATCH_SIZE = int(os.getenv("BATCH_SIZE", "4"))
-    LOCAL_CHECKPOINT_DIR = os.getenv("LOCAL_CHECKPOINT_DIR", "")
-    SAVED_MODEL_PATH = os.getenv("SAVED_MODEL_PATH", "")
-    SAVED_MODEL_PATH_FULL = os.getenv("SAVED_MODEL_PATH_FULL", "")
+    LOCAL_CHECKPOINT_DIR = os.getenv(
+        "LOCAL_CHECKPOINT_DIR", "/opt/ml/model/checkpoints"
+    )
+    SAVED_MODEL_PATH = os.getenv("SAVED_MODEL_PATH", "/opt/ml/model")
+    SAVED_MODEL_PATH_FULL = os.getenv(
+        "SAVED_MODEL_PATH_FULL", "/opt/ml/model/gpt.weights.keras"
+    )
